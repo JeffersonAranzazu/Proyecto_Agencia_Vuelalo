42de883356857df634f138f348ffdadf3f80fcc1
AlexandraRincon <alexandra.03.ar@gmail.com>
2021-10-12 17:02:48 -0500

    Avance Proyecto seccion ventas, files + html + mysql

---
+++ src/main/java/com/Proyecto/Agencia/Vuelalo/Interfaces/IFactura.java
@@ -0,0 +1,10 @@
+package com.Proyecto.Agencia.Vuelalo.Interfaces;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.Proyecto.Agencia.Vuelalo.Model.FacturaModel;
+
+
+public interface IFactura extends CrudRepository<FacturaModel, String>{
+
+}
---
+++ src/main/java/com/Proyecto/Agencia/Vuelalo/Interfaces/Service/IFacturaService.java
@@ -0,0 +1,10 @@
+package com.Proyecto.Agencia.Vuelalo.Interfaces.Service;
+
+import com.Proyecto.Agencia.Vuelalo.Model.FacturaModel;
+
+
+public interface IFacturaService {
+	public int saveFactura (FacturaModel f);
+
+	
+}
